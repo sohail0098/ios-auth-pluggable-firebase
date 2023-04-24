0ba15fbd21aefbe375689a6a2a23990e324ad151
sohail0098 <mohammedsohailahmed18@gmail.com>
2023-04-24 00:20:46 -0400

    Add login and signup using firebase auth

--- recipes/LoginViewController.swift
+++ recipes/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -16,6 +17,29 @@ class LoginViewController: UIViewController {
     }
     
 
+    @IBOutlet weak var loginEmail: UITextField!
+    
+    @IBOutlet weak var loginPassword: UITextField!
+    
+    
+    @IBOutlet weak var loginErrorLabel: UILabel!
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        let email = loginEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = loginPassword.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                self.loginErrorLabel.text = error!.localizedDescription
+                self.loginErrorLabel.alpha = 1
+            } else {
+                let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC") as? HomeViewController
+                self.view.window?.rootViewController = homeViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
--- recipes/RegisterViewController.swift
+++ recipes/RegisterViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -20,8 +22,43 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var registerEmailAddress: UITextField!
     @IBOutlet weak var registerPassword: UITextField!
     @IBOutlet weak var registerPasswordConfirm: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     @IBAction func registerButton(_ sender: UIButton) {
+        if registerFullName.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            registerEmailAddress.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            registerPassword.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            registerPasswordConfirm.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            errorLabel.text = "Please fill all the fields properly"
+            errorLabel.alpha = 1
+        } else if (registerPassword.text?.trimmingCharacters(in: .whitespacesAndNewlines) != registerPasswordConfirm.text?.trimmingCharacters(in: .whitespacesAndNewlines)) {
+            errorLabel.text = "Passwords Do Not Match!"
+            errorLabel.alpha = 1
+        } else {
+            let name = registerFullName.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = registerEmailAddress.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = registerPassword.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                if err != nil {
+                    self.errorLabel.text = "Error Creating User Account!"
+                    self.errorLabel.alpha = 1
+                } else {
+                    let db = Firestore.firestore()
+                    db.collection("users").addDocument(data: ["name": name, "uid": result!.user.uid]) { (error) in
+                        if error != nil {
+                            self.errorLabel.text = "Error saving user data"
+                            self.errorLabel.alpha = 1
+                        }
+                    }
+                    let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC") as? HomeViewController
+                    self.view.window?.rootViewController = homeViewController
+                    self.view.window?.makeKeyAndVisible()
+                }
+            }
+            
+        }
+        
     }
     /*
     // MARK: - Navigation
